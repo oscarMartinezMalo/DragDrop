db2c9b54051855f3bc0b01e05f8a22bf31bb81dd
Oscar Martinez <omartinezz1988@gmail.com>
2016-05-24 10:43:35 -0400

    drag and drop working

---
+++ dragDrop/dragDrop/Scripts/JavaScript.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="_references.js" />
+/// <reference path="jquery-2.2.3.js" />
+
+$(document).ready(function () {
+    $('.object').on('dragstart', dragging);
+    //$('.object').on('dragend', draggingEnded);
+    //$('.cover').on('dragenter', preventDefault);
+    //$('.cover').on('dragover', preventDefault);
+    $('.cover').on('drop', droppingItem);
+});
+
+var $draggedItem;
+
+function dragging(e) {
+    $(e.target).addClass("draggingNow");
+    $draggedItem = $(e.target);
+}
+
+function draggingEnded(e) {
+    $(e.target).removeClass('draggingNow');
+}
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function droppingItem(e) {
+
+    var cover = $(e.target);    
+    if (cover.hasClass('cover') && cover.children().length==0) {
+        $draggedItem.detach(); 
+        $draggedItem.appendTo($(e.target));
+    }
+}
